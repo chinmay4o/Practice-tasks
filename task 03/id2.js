2cc214e50c4cf5edba0cdf4b76a45fde63ea06fa
chinmay surve <56297843+chinmay4o@users.noreply.github.com>
2021-04-26 11:12:45 +0530

    Add files via upload

---
+++ task 03/id2.js
@@ -0,0 +1,35 @@
+
+var a = [1,2,3,4,5];
+var k =2;
+
+for(let i=0;i<k;i++){
+    let poped = a.pop();
+    a.unshift(poped);
+    }
+
+console.log(a);
+
+///
+
+let a1 = [1,2,3,3,4]
+  var set1 = new Set(a1)
+  
+   //s2.push(set1[0]);
+
+  console.log(set1);
+
+
+  //
+
+  function play(x,y){
+    var x = parseInt(prompt("first num?"));
+    var y = +prompt("sec num?")
+
+   console.log(x+y);
+
+  }
+
+  play(34,45);
+
+
+  //
